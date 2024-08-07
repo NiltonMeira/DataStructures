7a7bdf0832387b1e8a3ec22217e73a1c0358121a
Nilton <niltonprudys@gmail.com>
2024-08-06 21:05:54 -0300

    feat: queue push function

--- queues/main.c
+++ queues/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+int queue[];
+int head = -1;
+int tail = 1;
+
+void push();
+
 int main(void)
 {
-    printf("Hello, World!\n");
-    return 0;
+    int userInput;
+    printf("Hello buddy!, this is my queue.\nWhat do you want to do ?");
+    printf("\n1 - Push");
+    scanf("%d", &userInput);
+
+    switch (userInput) {
+        case 1:
+            int number;
+            printf("Number: ");
+            scanf("%d", &number);
+            push(number);
+    }
+}
+
+void push(int value)
+{
+    head ++;
+    queue[head] = value;
+    printf("The value %d was pushed!\n", value);
 }
