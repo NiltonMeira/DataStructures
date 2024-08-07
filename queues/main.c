fdc6a129f9bba90bec4a761e2ac2369bc8e2ed57
Nilton <niltonprudys@gmail.com>
2024-08-06 21:37:15 -0300

    feat: pop and show functions

--- queues/main.c
+++ queues/main.c
@@ -5,9 +5,10 @@ int head = -1;
 int tail = 1;
 
 void push();
+void pop();
+void show();
 
-int main(void)
-{
+int main(void) {
     int userInput;
     printf("Hello buddy!, this is my queue.\nWhat do you want to do ?");
     printf("\n1 - Push");
@@ -19,12 +20,34 @@ int main(void)
             printf("Number: ");
             scanf("%d", &number);
             push(number);
+            break;
+
+        case 2:
+            pop();
+            printf("Poped");
+            break;
+
+        case 3:
+            show();
+            break;
     }
 }
 
-void push(int value)
-{
-    head ++;
+void push(int value) {
+    head++;
     queue[head] = value;
     printf("The value %d was pushed!\n", value);
+}
+
+void pop() {
+    for (int i = 0; i < head; i++) {
+        queue[i] = queue[i+1];
+    }
+    head --;
+}
+
+void show() {
+    for(int i =0; i < head;i++) {
+        printf("[%d]\n", queue[i]);
+    }
 }
